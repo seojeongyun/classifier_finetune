4e2e7b67dbcb82887fbd83bd6fa7bff712ea9807
seojeongyun <tjwjddbs4018@naver.com>
2023-10-02 19:29:41 +0900

    .

---
+++ data_loader.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+
+cudnn.benchmark = True
+plt.ion()   # interactive mode
+
+class dataloader:
+    def __init__(self):
+        self.path = '/storage/jysuh/hymenoptera_data'
+        self.transform = self.data_transfrom()
+        self.image_datasets = self.get_datasets()
+        self.dataloaders = self.get_loaders()
+        self.len = self.get_len()
+        self.cls_name = self.get_clsname()
+
+    def data_transfrom(self):
+        data_transforms = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        }
+        return data_transforms
+
+    def get_datasets(self):
+        image_datasets = {x: datasets.ImageFolder(os.path.join(self.path, x),self.transform[x]) for x in ['train', 'val']}
+        return image_datasets
+
+    def get_loaders(self):
+        dataloaders = {x: torch.utils.data.DataLoader(self.image_datasets[x],
+                                                      batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
+        return dataloaders
+
+    def get_len(self):
+        dataset_sizes = {x: len(self.image_datasets[x]) for x in ['train', 'val']}
+        return dataset_sizes
+
+    def get_clsname(self):
+        class_names = self.image_datasets['train'].classes
+        return class_names
---
+++ train.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+from data_loader import dataloader
+from function import imshow
+from engine import Trainer
+
+cudnn.benchmark = True
+plt.ion()   # interactive mode
+
+
+
+
+if __name__ == '__main__':
+    data = dataloader()
+    trainer = Trainer(epochs=10, num_images=6)
+
+    # Get a batch of training data
+    inputs, classes = next(iter(data.dataloaders['train']))
+
+    # Make a grid from batch
+    out = torchvision.utils.make_grid(inputs)
+
+    imshow(out, title=[data.cls_name[x] for x in classes])
+
+    model_ft = trainer.train_model()
+
+    trainer.visualize_model()
