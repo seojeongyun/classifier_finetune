8570f048c0e96dac5b62b3398679f922198e3c6e
seojeongyun <tjwjddbs4018@naver.com>
2023-10-03 16:45:28 +0900

    .

--- engine/engine_freeze.py
+++ engine/engine_freeze.py
@@ -3,14 +3,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 import torch.backends.cudnn as cudnn
-import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
-import os
 from PIL import Image
-from tempfile import TemporaryDirectory
 from data_loader import dataloader
 from function import imshow
 
